5c2969368c7a48576d8e92b725560c531b9f420f
sushiljainam <sushiljain1993@gmail.com>
2016-11-23 12:26:38 +0530

    stable 1 - working

--- submit/index.php
+++ submit/index.php
@@ -1,5 +1,21 @@
 <?php
 
-echo "reply from website - ";
+echo "reply from website - \n";
+if (isset($_POST)) {
+	if (isset($_POST['u_email'])) {
+		if(isset($_POST['u_passwd'])) {
+			//either sign up or login.
+			$email = $_POST['u_email'];
+			$passwd = $_POST['u_passwd'];
+			echo "received inputs as - \n";
+			echo $email." \n";
+			echo $passwd." \n";
+
+			//$var_dump($_POST);
+
+			//save data in file
+		}
+	}
+}
 
 ?>
