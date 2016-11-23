c4e147f7ddcc7c7591234961bf4463693d6bc743
sushiljainam <sushiljain1993@gmail.com>
2016-11-23 16:18:22 +0530

    its still working

--- submit/index.php
+++ submit/index.php
@@ -1,21 +1,30 @@
 <?php
 
 $filename = 'req.data';
-
-echo "reply from website - \n";
+echo json_encode(get_defined_vars());
+// echo "reply from website - \n";
 if (isset($_POST)) {
 	if (isset($_POST['u_email'])) {
 		if(isset($_POST['u_passwd'])) {
 			//either sign up or login.
 			$email = $_POST['u_email'];
 			$passwd = $_POST['u_passwd'];
-			echo "received inputs as - \n";
-			echo $email." \n";
-			echo $passwd." \n";
+			// echo "sent data as - \n";
+			// echo $email." \n";
+			// echo $passwd." \n";
 
 			$success = saveRequest($filename,'signup',[$email,$passwd]);
 
-			echo $success." \n";
+			echo $success;
+			// if ($success > 0) {
+			// 	echo "done successfully \n";
+			// } else {
+			// 	echo "not done successfully \n";
+			// 	if ($success === false) {
+			// 		echo "success is myth \n";
+			// 	}
+			// }
+			
 			//if success > 0 OK
 			//if success === false -> not OK
 			
@@ -27,7 +36,7 @@ if (isset($_POST)) {
 
 function saveRequest($filename, $type, $data)
 {
-	$stringToWrt = $type.' : '.$data[0].','.$data[1].'\n';
+	$stringToWrt = $type.' : '.$data[0].','.$data[1].'||';
 	$flag = file_put_contents ( $filename, $stringToWrt, FILE_APPEND );
 	return $flag;
 }
