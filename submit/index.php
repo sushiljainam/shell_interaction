9cdb07f868726e8150f75d570c09b72c68e80f9b
sushiljainam <sushiljain1993@gmail.com>
2016-11-24 16:50:27 +0530

    log - headers changes

--- submit/index.php
+++ submit/index.php
@@ -1,7 +1,10 @@
 <?php
 
 $filename = 'req.data';
-echo json_encode(get_defined_vars());
+//echo json_encode(get_defined_vars());
+//print_r(array_keys(get_defined_vars()));
+print_r(get_defined_vars());
+
 // echo "reply from website - \n";
 if (isset($_POST)) {
 	if (isset($_POST['u_email'])) {
@@ -14,7 +17,7 @@ if (isset($_POST)) {
 			// echo $passwd." \n";
 
 			$success = saveRequest($filename,'signup',[$email,$passwd]);
-
+                                     saveLog('log.data','log'.$email);
 			echo $success;
 			// if ($success > 0) {
 			// 	echo "done successfully \n";
@@ -38,6 +41,13 @@ function saveRequest($filename, $type, $data)
 {
 	$stringToWrt = $type.' : '.$data[0].','.$data[1].'||';
 	$flag = file_put_contents ( $filename, $stringToWrt, FILE_APPEND );
+	return $flag;
+}
+function saveLog($filename, $type)
+{
+	$stringToWrt = $type.' : '.'||';
+	$flag = file_put_contents ( $filename, $stringToWrt, FILE_APPEND );
+                file_put_contents ( $filename, print_r(get_defined_vars()), FILE_APPEND);
 	return $flag;
 }
 ?>
